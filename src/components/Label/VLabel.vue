0efff946e98b0eea50fafa607515075ae0c6479e
Alexandre Teixeira Mendes <alex.fnvip@gmail.com>
2025-11-28 14:34:09 -0400

    WIP

--- src/components/Label/VLabel.vue
+++ src/components/Label/VLabel.vue
@@ -2,9 +2,9 @@
   <label class="v-label">
     <span class="titulo" v-html="label" :title="obrigatorio ? 'Este campo é obrigatório' : ''" />
 
-    <span v-if="obrigatorio" class="text-red-600">*</span>
+    <span v-if="obrigatorio" class="text-red-6">*</span>
 
-    <q-icon v-if="ajuda" name="help_outline" size="16px" class="q-ml-xs cursor-pointer text-grey-7">
+    <q-icon v-if="ajuda" name="help_outline" size="16px" class="q-ml-sm cursor-pointer ">
       <q-tooltip>{{ ajuda }}</q-tooltip>
     </q-icon>
   </label>
@@ -31,16 +31,12 @@ defineProps({
 .v-label {
   display: inline-flex;
   align-items: center;
-  font-weight: 500;
-  font-size: 14px;
-  gap: 4px;
+  /* font-weight: 500; */
+  /* font-size: 14px; */
 }
 
 .titulo {
-  font-weight: 600;
-  text-transform: uppercase;
-  font-size: 0.75rem;
-  letter-spacing: 0.5px;
-  color: #37474f;
+  font-size: 14px;
+  font-weight: normal;
 }
 </style>
